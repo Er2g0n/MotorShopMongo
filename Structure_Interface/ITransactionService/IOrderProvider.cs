2704bb444b35420c1436ae777348dc8bf7076daa
Eragon <hnguyenngoc.h@gmail.com>
2025-07-23 11:47:55 +0700

    chore: refactor code

---
+++ Structure_Interface/ITransactionService/IOrderProvider.cs
@@ -0,0 +1,9 @@
+﻿using Structure_Core.BaseClass;
+using Structure_Core.TransactionManagement;
+
+
+namespace Structure_Interface.ITransactionService;
+public interface IOrderProvider
+{
+    Task<ResultService<IEnumerable<Order>>> Search(string? keyword = null, string? orderBy = null, DateTime? startDate = null, DateTime? endDate = null);
+}
